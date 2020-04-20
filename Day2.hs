be10e9dd968d889c0e57093d4afe4c14c270907a
Thomas Koster <tkoster@gmail.com>
2020-04-20 10:49:39 +1000

    Clean up day 2

--- Day2.hs
+++ Day2.hs
@@ -1,9 +1,7 @@
-module Main where
+module Day2 where
 
-import Control.Exception (assert)
 import Control.Monad (when)
-import Data.List (nub)
-import Data.Vector.Unboxed ((!), fromList, update)
+import Data.Vector.Unboxed (Vector, (!), fromList, update)
 import System.Exit (exitFailure)
 
 import Intcode
@@ -14,12 +12,14 @@ main = do
   part1
   part2
 
+part1 :: IO ()
 part1 = do
   input <- readFile "day2input.txt"
   let program = Intcode.parse input
       result  = run program 12 2
   print (result ! 0)
 
+part2 :: IO ()
 part2 = do
   input <- readFile "day2input.txt"
   let program = Intcode.parse input
@@ -32,11 +32,13 @@ part2 = do
   let ((noun, verb, _) : _) = results
   print (100 * noun + verb)
 
+run :: Vector Int -> Int -> Int -> Vector Int
 run program noun verb =
   let program' = update program $ fromList [(1, noun), (2, verb)]
       result = Intcode.exec program'
   in  result
 
+selfTest :: IO ()
 selfTest = do
   testExec "1,9,10,3,2,3,11,0,99,30,40,50" "3500,9,10,70,2,3,11,0,99,30,40,50"
   testExec "1,0,0,0,99" "2,0,0,0,99"
@@ -44,6 +46,7 @@ selfTest = do
   testExec "2,4,4,5,99,0" "2,4,4,5,99,9801"
   testExec "1,1,1,4,99,5,6,0,99" "30,1,1,4,2,5,6,0,99"
 
+testExec :: String -> String -> IO ()
 testExec input expected = do
   let actual = (exec . parse) input
   when (actual /= parse expected) $ do
